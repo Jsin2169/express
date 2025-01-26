8beebd3eaa7c5f2a691c9435c182517e128992f4
Jsin2169 <muneyoshi1980@gmail.com>
2025-01-26 19:14:07 +0900

    add env

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,11 +1,12 @@
  var express = require('express');
  var router = express.Router();
-
+ const cors = require('cors'); // corsミドルウェアを追加
+ require('dotenv').config();
 
 
  // 接続情報を設定
  const { MongoClient } = require("mongodb");
- const uri = "mongodb+srv://jsinmongo:cumongojsin@cluster0.lhpkx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+ const uri = process.env.MONGODB_URI;
  const client = new MongoClient(uri);
 
  router.get('/', async (req, res) => {
@@ -13,12 +14,9 @@
  const database = client.db('notes');
  const notes = database.collection('notes');
 
- // idが１のドキュメントを取得
- //const query = { id: 2 };
- //const note = await notes.findOne(query);
-
  // 全てのドキュメントを取得
-const note = await notetes.find({}).toArray();
+const note = await notes.find({}).toArray();
+
 
  res.json(note);
  })
